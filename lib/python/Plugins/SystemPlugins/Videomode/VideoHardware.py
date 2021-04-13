49fc03d41c0681c8c15abd4aef4d1c372913e686
tnap-builder <79226521+tnap-builder@users.noreply.github.com>
2021-04-13 07:57:21 -0400

    Add files via upload

--- lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
+++ lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
@@ -16,73 +16,49 @@ class VideoHardware:
 
 	modes = { }  # a list of (high-level) modes for a certain port.
 
-	rates["PAL"] =			{ "50Hz":	{ 50: "pal" },
-								"60Hz":		{ 60: "pal60" },
-								"multi":	{ 50: "pal", 60: "pal60" } }
 
 	rates["NTSC"] =			{ "60Hz": 	{ 60: "ntsc" } }
 
-	rates["Multi"] =		{ "multi": 	{ 50: "pal", 60: "ntsc" } }
-
 	rates["480i"] =			{ "60Hz": 	{ 60: "480i" } }
-
-	rates["576i"] =			{ "50Hz": 	{ 50: "576i" } }
 
 	rates["480p"] =			{ "60Hz": 	{ 60: "480p" } }
 
-	rates["576p"] =			{ "50Hz": 	{ 50: "576p" } }
-
-	rates["720p"] =			{ "50Hz": 	{ 50: "720p50" },
+	rates["720p"] =			{ "60Hz": 	{ 60: "720p" },
 								"60Hz": 	{ 60: "720p" },
-								"multi": 	{ 50: "720p50", 60: "720p" },
-								"auto":		{ 50: "720p50", 60: "720p", 24: "720p24" } }
+								"auto":		{ 60: "720p" } }
 
 	rates["1080i"] =		{ "50Hz":	{ 50: "1080i50" },
 								"60Hz":		{ 60: "1080i" },
-								"multi":	{ 50: "1080i50", 60: "1080i" },
 								"auto": 	{ 50: "1080i50", 60: "1080i", 24: "1080p24" } }
 
 	rates["1080p"] =		{ "50Hz":	{ 50: "1080p50" },
 								"60Hz":		{ 60: "1080p" },
-								"multi":	{ 50: "1080p50", 60: "1080p" },
 								"auto":		{ 50: "1080p50", 60: "1080p", 24: "1080p24" } }
 
 	rates["2160p30"] =		{ "25Hz":	{ 50: "2160p25" },
 								"30Hz":		{ 60: "2160p30" },
-								"multi":	{ 50: "2160p25", 60: "2160p30" },
 								"auto":		{ 50: "2160p25", 60: "2160p30", 24: "2160p24" } }
 
 	rates["2160p"] =		{ "50Hz":	{ 50: "2160p50" },
 								"60Hz":		{ 60: "2160p" },
-								"multi":	{ 50: "2160p50", 60: "2160p" }, 
 								"auto":		{ 50: "2160p50", 60: "2160p", 24: "2160p24" }}
 
 	rates["PC"] = {
 		"1024x768": { 60: "1024x768" }, # not possible on DM7025
 		"800x600" : { 60: "800x600" },  # also not possible
 		"720x480" : { 60: "720x480" },
-		"720x576" : { 60: "720x576" },
 		"1280x720": { 60: "1280x720" },
-		"1280x720 multi": { 50: "1280x720_50", 60: "1280x720" },
 		"1920x1080": { 60: "1920x1080"},
-		"1920x1080 multi": { 50: "1920x1080", 60: "1920x1080_50" },
 		"1280x1024" : { 60: "1280x1024"},
 		"1366x768" : { 60: "1366x768"},
-		"1366x768 multi" : { 50: "1366x768", 60: "1366x768_50" },
 		"1280x768": { 60: "1280x768" },
 		"640x480" : { 60: "640x480" }
 	}
 
-	if SystemInfo["HasScart"]:
-		modes["Scart"] = ["PAL", "NTSC", "Multi"]
-	elif SystemInfo["HasComposite"]:
-		modes["RCA"] = ["576i", "PAL", "NTSC", "Multi"]
-	if SystemInfo["HasYPbPr"]:
-		modes["YPbPr"] = ["720p", "1080i", "576p", "480p", "576i", "480i"]
 	if SystemInfo["Has2160p"]:
-		modes["DVI"] = ["720p", "1080p", "2160p", "1080i", "576p", "480p", "576i", "480i"]
+		modes["DVI"] = ["720p", "1080p", "2160p", "1080i", "480p", "480i"]
 	else:
-		modes["DVI"] = ["720p", "1080p", "2160p", "2160p30", "1080i", "576p", "480p", "576i", "480i"]
+		modes["DVI"] = ["720p", "1080p", "2160p", "2160p30", "1080i", "480p", "480i"]
 	modes["DVI-PC"] = ["PC"]
 
 	def getOutputAspect(self):
@@ -217,7 +193,7 @@ class VideoHardware:
 				print "[VideoHardware] setting videomode failed."
 
 		try:
-			open("/etc/videomode", "w").write(mode_50) # use 50Hz mode (if available) for booting
+			open("/etc/videomode", "w").write(mode_60) # use 50Hz mode (if available) for booting
 		except IOError:
 			print "[VideoHardware] writing initial videomode to /etc/videomode failed."
 
