8f0f5bf99752eb141ca663c5dfab99108dff1990
tnap-builder <79226521+tnap-builder@users.noreply.github.com>
2024-05-09 21:20:47 -0400

    Add files via upload

--- lib/python/Plugins/Extensions/MediaPlayer/plugin.py
+++ lib/python/Plugins/Extensions/MediaPlayer/plugin.py
@@ -20,7 +20,7 @@ from Components.ServiceEventTracker import ServiceEventTracker, InfoBarBase
 from Components.Playlist import PlaylistIOInternal, PlaylistIOM3U, PlaylistIOPLS
 from Components.AVSwitch import AVSwitch
 from Components.config import config
-from Components.SystemInfo import SystemInfo
+from Components.SystemInfo import BoxInfo
 from Tools.Directories import fileExists, resolveFilename, SCOPE_CONFIG, SCOPE_PLAYLIST, SCOPE_CURRENT_SKIN
 from Tools.BoundFunction import boundFunction
 from Plugins.Extensions.MediaPlayer.settings import MediaPlayerSettings
@@ -130,6 +130,8 @@ class MediaPlayer(Screen, InfoBarBase, InfoBarScreenSaver, InfoBarSeek, InfoBarA
 
 		# 'None' is magic to start at the list of mountpoints
 		defaultDir = config.mediaplayer.defaultDir.getValue()
+		if defaultDir is not None and not os.path.isdir(defaultDir):
+			defaultDir = None
 		self.filelist = FileList(defaultDir, matchingPattern="(?i)^.*\.(dts|mp3|wav|wave|wv|oga|ogg|flac|m4a|mp2|m2a|wma|ac3|mka|aac|ape|alac|mpg|vob|m4v|mkv|avi|divx|dat|flv|mp4|mov|wmv|asf|3gp|3g2|mpeg|mpe|rm|rmvb|ogm|ogv|m2ts|mts|ts|m3u|e2pls|pls|amr|au|mid|pva|wtv)", useServiceRef=True, additionalExtensions="4098:m3u 4098:e2pls 4098:pls")
 		self["filelist"] = self.filelist
 
@@ -265,7 +267,7 @@ class MediaPlayer(Screen, InfoBarBase, InfoBarScreenSaver, InfoBarSeek, InfoBarA
 		if InfoBar.instance is not None:
 			self.servicelist = InfoBar.instance.servicelist
 			if self.servicelist and hasattr(self.servicelist, 'dopipzap'):
-				self.pipZapAvailable = SystemInfo.get("NumVideoDecoders", 1) > 1
+				self.pipZapAvailable = BoxInfo.getItem("NumVideoDecoders", 1) > 1
 
 	def prevBouquetHelpText(self):
 		if not self.shown and self.isPiPzap():
