8eea376a95b4a0fdf6985fa3a15ab24cf3c1c6c3
tnap-builder <79226521+tnap-builder@users.noreply.github.com>
2024-05-27 00:06:05 -0400

    Add files via upload

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1932,7 +1932,7 @@ int eDVBFrontend::tuneLoopInt()  // called by m_tuneTimer
 			case eSecCommand::START_TUNE_TIMEOUT:
 			{
 				int tuneTimeout = (m_sec_sequence.current()->timeout);
-				if (event.status & FE_HAS_LOCK)
+				if (FE_HAS_LOCK)
 					eDebugNoSimulate("[eDVBFrontend%d] startTuneTimeout %d", m_dvbid, tuneTimeout);
 				if (!m_simulate)
 					m_timeout->start(tuneTimeout, 1);
