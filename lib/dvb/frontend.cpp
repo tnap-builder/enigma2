de49d8d5bde6911306ee69b40d6083fd3a9bea40
tnap-builder <79226521+tnap-builder@users.noreply.github.com>
2024-05-29 11:25:14 -0400

    Add files via upload

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1937,15 +1937,15 @@ int eDVBFrontend::tuneLoopInt()  // called by m_tuneTimer
 				int tuneTimeout = (m_sec_sequence.current()->timeout);
 				int allowunlock = (eConfigManager::getConfigBoolValue(allow_unlocked_transponder));
 				eDebugNoSimulate("[eDVBFrontend%d] startTuneTimeout %d, lockstat =  %d", m_dvbid, tuneTimeout, lockstat);
-				if (!m_simulate && allowunlock == 1)
-				{
-					m_timeout->start(tuneTimeout, 1);				
-					eDebug("[eDVBFrontend%d] allowunlock == 1  Timeout = %d, lockstat =  %d allowunlock = %d", m_dvbid, tuneTimeout, lockstat, allowunlock);
-				}
 				if (!m_simulate && lockstat == 0 && allowunlock == 0)
 				{
 					m_timeout->start((4000), 1);
 					eDebug("[eDVBFrontend%d] UNLOCKED TRANSPONDER  Timeout = %d, lockstat =  %d allowunlock = %d" , m_dvbid, tuneTimeout, lockstat, allowunlock);
+				}
+				if (!m_simulate && allowunlock == 1)
+				{
+					m_timeout->start(tuneTimeout, 1);				
+					eDebug("[eDVBFrontend%d] allowunlock == 1  Timeout = %d, lockstat =  %d allowunlock = %d", m_dvbid, tuneTimeout, lockstat, allowunlock);
 				}
 				else if (!m_simulate)
 				{
