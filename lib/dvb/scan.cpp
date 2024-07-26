2f32ba521be653333d9c4ec7abf8b8e71b35f745
tnap-builder <79226521+tnap-builder@users.noreply.github.com>
2024-07-26 14:48:29 -0400

    Add files via upload

--- lib/dvb/scan.cpp
+++ lib/dvb/scan.cpp
@@ -1049,7 +1049,7 @@ void eDVBScan::channelDone()
 	}
 
 	if (!m_chid_current)
-		eWarning("[scan.cpp-#1025] the current channel's ID was not corrected - not adding channel %d %d", m_chid_current, m_ch_current);
+		eWarning("[scan.cpp-#1025] the current channel's ID was not corrected - not adding channel %s %s", m_chid_current, m_ch_current);
 	else
 	{
 		addKnownGoodChannel(m_chid_current, m_ch_current);
